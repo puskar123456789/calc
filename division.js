eeec2ff9fbfb1bb686b65fd34c1a6303f5193845
Gaurav Singh <s.kumar9608616450@gmail.com>
2022-07-01 15:29:21 +0530

    division functionality added

---
+++ division.js
@@ -0,0 +1,8 @@
+function div() {
+
+var firstno = document.getElementById("firstno").value
+var secondno = document.getElementById(secondno").value
+
+var result = firstno/secondno
+document.getElementById("result").value = result
+}
